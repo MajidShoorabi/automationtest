f954b49db263e6aad8ccd30ed70a0d7fe8b292c8
Majid <majidshoorabi@gmail.com>
2022-04-24 21:02:17 +0430

    refactor - rename package

--- src/main/java/com/github/majidshoorabi/automationtest/session14/Sample01.java
+++ src/main/java/com/github/majidshoorabi/automationtest/session14/Sample01.java
@@ -1,4 +1,4 @@
-package com.github.majidshoorabi.automationtext.session14;
+package com.github.majidshoorabi.automationtest.session14;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
